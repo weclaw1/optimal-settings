8c59a35bd20fc389a6f3ff07dbfa9d1de08623cc
Robert Węcławski <r.weclawski@gmail.com>
2023-12-28 22:00:24 +0100

    remove unused imports

--- optimal-settings-backend/src/models.rs
+++ optimal-settings-backend/src/models.rs
@@ -10,11 +10,9 @@ pub mod auth;
 
 pub use game::Game;
 pub use game_image::GameImage;
-pub use game_settings::GameSettings;
-pub use game_settings_sources::GameSettingsSources;
 pub use guide::Guide;
 pub use guide_image::GuideImage;
-pub use report::{OperatingSystem, Report};
+pub use report::Report;
 
 pub trait ValidateModel {
     fn validate(&self) -> Result<(), anyhow::Error>;
